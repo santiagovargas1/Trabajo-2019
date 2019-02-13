7d7481e3c171ebb53a5ebc98435303f532b53fcb
santiagovargas1 <47116685+santiagovargas1@users.noreply.github.com>
2019-02-13 15:48:52 -0500

    Add files via upload

---
+++ Arduino.ino
@@ -0,0 +1,19 @@
+int sensorPin = 2;
+int ledPin = 3;
+
+void setup() {
+ pinMode(sensorPin, INPUT);
+ pinMode(ledPin, OUTPUT);
+ 
+
+}
+
+void loop() {
+  if(digitalRead(sensorPin) == HIGH){
+    digitalWrite(ledPin, HIGH);
+  }else{
+    digitalWrite(ledPin, LOW);
+    
+    }
+
+}
