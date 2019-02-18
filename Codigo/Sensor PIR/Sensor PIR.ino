bbbee435c40ff50f00e39da83738fef6526db3e5
santiagovargas1 <47116685+santiagovargas1@users.noreply.github.com>
2019-02-18 11:58:32 -0500

    Update Sensor PIR.ino

--- Codigo/Sensor PIR/Sensor PIR.ino
+++ Codigo/Sensor PIR/Sensor PIR.ino
@@ -1,1 +1,14 @@
+int sensorPin = 2;
+int ledPin = 3;
 
+void setup() {
+ pinMode(sensorPin, INPUT);
+ pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  if(digitalRead(sensorPin) == HIGH){
+    digitalWrite(ledPin, HIGH);
+  }else{
+    digitalWrite(ledPin, LOW);    
+    }
+}
